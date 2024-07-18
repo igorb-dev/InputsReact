20cb2676d4bacfaaf502a383dbe39afbb303a6fe
igorb-dev <bitencourt.igor@hotmail.com>
2024-07-18 15:24:49 -0300

    edit header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import TextInput from './componentes/TextInput';
 
 function App() {
+  const [nome, setNome] = useState<string>("")
+
   return (
     <div className="App">
 
-      <p className='igorbdev'>{"<IgorB-Dev />"}</p>
+      <h1 className='igorbdev'>{"<IgorB-Dev />"}</h1>
       
-        <TextInput name='name' placeholder='Nome Completo' />
+        <TextInput name='name' placeholder='Nome Completo' value={nome} onChange={setNome} />
 
+      <p style={{marginTop: 40}}>Texto do input: {nome}</p>
 
     </div>
   );
--- src/componentes/TextInput.tsx
+++ src/componentes/TextInput.tsx
@@ -3,14 +3,22 @@ import "./TextInput.styles.scss"
 
 interface ITextInput {
   name: string,
-  placeholder: string
+  placeholder: string,
+  value: string,
+  onChange: (value: string) => void
 }
 
-const TextInput = ({name, placeholder}: ITextInput) => {
+const TextInput = ({name, placeholder, value, onChange}: ITextInput) => {
   return (
     <div className='TextInput'>
-        <input className='input' id={name} placeholder={placeholder}/>
-        <label className='label' htmlFor={name}>{placeholder}</label>
+      <input
+        className='input'
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <label className='label' htmlFor={name}>{placeholder}</label>
     </div>
   )
 }
