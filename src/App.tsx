6949cb36e4d5098fa7755cbe8a05f4c0bd3b9cf9
igorb-dev <bitencourt.igor@hotmail.com>
2024-07-18 15:27:51 -0300

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
 import './App.scss';
-import TextInput from './componentes/TextInput';
 
 function App() {
-  const [nome, setNome] = useState<string>("")
-
   return (
     <div className="App">
 
       <h1 className='igorbdev'>{"<IgorB-Dev />"}</h1>
       
-        <TextInput name='name' placeholder='Nome Completo' value={nome} onChange={setNome} />
-
-      <p style={{marginTop: 40}}>Texto do input: {nome}</p>
-
     </div>
   );
 }
